20989a338cb01ba8c306fbdc6eafb10d4a46b05e
fragata-adrian <fragataadrian99@gmail.com>
2020-12-03 20:31:28 -0300

    Sistema base modificado

--- sistema-base.py
+++ sistema-base.py
@@ -1,35 +1,51 @@
 import time
 import random
+import threading
 
-# Sistema base con el que cuenta la empresa actualmente
-# Este sistema no es concurrente
+'''
+  Sistema base con el que cuenta la empresa actualmente
+  Este sistema no es concurrente
+'''
 
 muestras = []
-cantidadDeMuestras = 5
+resultados = []
+cantidadDeMuestras = 15
 tiempoTotal = 0
+
+# Contador de tiempo de analisis
+
+def contador():
+  global tiempoTotal, cantidadDeMuestras, resultados
+
+  while len(resultados) < cantidadDeMuestras:
+    time.sleep(1)
+    tiempoTotal += 1
+  print(">>> Se analizaron", cantidadDeMuestras, "muestras en un tiempo total de", tiempoTotal, "dias.")
+
 
 # Unidad de analisis actual
 
 def unidadDeAnalisis():
-  global tiempoTotal
+  global muestras, resultados
   tiempoDeAnalisis = random.randint(1, 15)
-  tiempoTotal += tiempoDeAnalisis
 
   muestras.pop(0)
-  print("> Analizando una muestra...")
+  print("> Analizando una muestra. Quedan", len(muestras))
   time.sleep(tiempoDeAnalisis)
-  print("> Muestra analizada!\n")
+  print("> Muestra analizada! Almacenando resultado\n")
+  resultados.append(1)
 
 # Agrego las muestras a analizar
 
 for i in range(cantidadDeMuestras):
   muestras.append(1)
 
+# Inicio el contador
+
+contador = threading.Thread(target=contador)
+contador.start()
+
 # Examino las muestras
 
 while len(muestras) > 0:
   unidadDeAnalisis()
-
-# Imprimo el tiempo total para analizar todas las muestras
-
-print("> Se analizaron", cantidadDeMuestras, "muestras en un tiempo total de", tiempoTotal, "dias.")
